e80ca99d8cad406ad0a0889c5a454d5be54a0fa8
Chris Gibson <chrischarlesgibson@gmail.com>
2023-04-05 12:31:20 -0500

    added login componenet logic, styling

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [];
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { MovieComponent } from './movie/movie.component';
+const routes: Routes = [
+  //This route matches the default path (i.e., the root URL). It redirects to the login path, which will cause the LoginComponent to be loaded.
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  //This route matches the login path. When this path is navigated to, the LoginComponent will be loaded.
+  {
+    path: 'login',
+    component: LoginComponent,
+  },
+  //This route matches the home path. When this path is navigated to, the HomeComponent will be loaded.
+  { path: 'home', component: HomeComponent },
+  //this route matches the movie path. When this path is navigated to, the MovieComponent will be loaded.
+  { path: 'movie', component: MovieComponent },
+  //This is a wildcard route that matches any URL that doesn't match the other routes defined in the array. It will cause the LoginComponent to be loaded for any unmatched URLs.
+  { path: '**', component: LoginComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
